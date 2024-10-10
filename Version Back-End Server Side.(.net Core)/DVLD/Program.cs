c450d5bce886fa14773fe7dd9ef0685af7fe03f5
unknown <mohammedabdelkrimguendouz@gmail.com>
2024-10-10 08:45:00 +0100

    next commit

--- Version Back-End Server Side.(.net Core)/DVLD/Program.cs
+++ Version Back-End Server Side.(.net Core)/DVLD/Program.cs
@@ -1,4 +1,4 @@
-
+﻿
 
     var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +8,8 @@
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
+
+    
 
     var app = builder.Build();
 
--- Version Back-End Server Side.(.net Core)/DVLD/obj/Debug/net8.0/DVLD.AssemblyInfo.cs
+++ Version Back-End Server Side.(.net Core)/DVLD/obj/Debug/net8.0/DVLD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DVLD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef7e017dda08b1a2e42181a2e2545ffd27f8fdb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DVLD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DVLD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
